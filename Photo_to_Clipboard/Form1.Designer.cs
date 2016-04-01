78ce6a57eb1736cb433326ea67ec57f37fc1c653
rodneywells01 <rodneywells01@gmail.com>
2016-04-01 02:38:31 -0400

    Here's the init push!

---
+++ Photo_to_Clipboard/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Photo_to_Clipboard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSetClipboard = new System.Windows.Forms.Button();
+            this.textBoxDesiredText = new System.Windows.Forms.TextBox();
+            this.labelInformation = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonSetClipboard
+            // 
+            this.buttonSetClipboard.Location = new System.Drawing.Point(35, 197);
+            this.buttonSetClipboard.Name = "buttonSetClipboard";
+            this.buttonSetClipboard.Size = new System.Drawing.Size(203, 53);
+            this.buttonSetClipboard.TabIndex = 0;
+            this.buttonSetClipboard.Text = "Set Clipboard";
+            this.buttonSetClipboard.UseVisualStyleBackColor = true;
+            this.buttonSetClipboard.Click += new System.EventHandler(this.buttonSetClipboard_Click);
+            // 
+            // textBoxDesiredText
+            // 
+            this.textBoxDesiredText.Location = new System.Drawing.Point(58, 52);
+            this.textBoxDesiredText.Name = "textBoxDesiredText";
+            this.textBoxDesiredText.Size = new System.Drawing.Size(158, 20);
+            this.textBoxDesiredText.TabIndex = 1;
+            // 
+            // labelInformation
+            // 
+            this.labelInformation.AutoSize = true;
+            this.labelInformation.Location = new System.Drawing.Point(114, 126);
+            this.labelInformation.Name = "labelInformation";
+            this.labelInformation.Size = new System.Drawing.Size(35, 13);
+            this.labelInformation.TabIndex = 2;
+            this.labelInformation.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.labelInformation);
+            this.Controls.Add(this.textBoxDesiredText);
+            this.Controls.Add(this.buttonSetClipboard);
+            this.Name = "Form1";
+            this.Text = "Photo to Clipboard!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSetClipboard;
+        private System.Windows.Forms.TextBox textBoxDesiredText;
+        private System.Windows.Forms.Label labelInformation;
+    }
+}
+
---
+++ Photo_to_Clipboard/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+
+Useful links:
+
+- C# Api 
+https://github.com/onedrive/onedrive-sdk-csharp
+
+- One Drive SDK
+https://dev.onedrive.com/sdks.htm
+
+
+
+
+
+*/
+
+
+
+namespace Photo_to_Clipboard
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public System.Drawing.Image DownloadImageFromUrl(string imageUrl)
+        {
+            /*
+                Eternal thanks to Forget Code.com for this function. 
+                http://forgetcode.com/CSharp/2052-Download-images-from-a-URL       
+             */
+
+            System.Drawing.Image image = null;
+            try
+            {
+                System.Net.HttpWebRequest webRequest = (System.Net.HttpWebRequest)System.Net.HttpWebRequest.Create(imageUrl);
+                webRequest.AllowWriteStreamBuffering = true;
+                webRequest.Timeout = 30000;
+
+                System.Net.WebResponse webResponse = webRequest.GetResponse();
+
+                System.IO.Stream stream = webResponse.GetResponseStream();
+
+                image = System.Drawing.Image.FromStream(stream);
+
+                webResponse.Close();
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+
+            return image;
+        }
+
+        private void buttonSetClipboard_Click(object sender, EventArgs e)
+        {
+            string desiredText = textBoxDesiredText.Text;
+            Clipboard.SetText(desiredText);
+            string imagelocation = "http://i.imgur.com/4DDzfxa.jpg";
+            // imagelocation = "C:/Users/Rodney/Desktop/W9OTW.jpg";
+            Image mypic = DownloadImageFromUrl(imagelocation);  
+            Clipboard.SetImage(mypic);
+            labelInformation.Text = "Image has been set!";
+        }
+
+        
+    }
+
+
+
+}
+
+
